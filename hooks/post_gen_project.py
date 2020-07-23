e403bda9c79bf87bf14f2bded77f890f804da1be
Jswig <andersjopo@gmail.com>
2020-07-23 11:18:38 -0700

    sanited variable names

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,7 @@ import shutil
 
 os_license = '{{ cookiecutter.license }}'
 notebooks = '{{ cookiecutter['jupyter notebooks'] }}'
+containers = '{{ cookitcutter.containers }}'
 
 if os_license == "No license file":
     os.remove("LICENSE")
